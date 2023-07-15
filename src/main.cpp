85121269587eeac3ff8cb7f3555bb727888372d7
RealAhani <alirezaahanu1377@gmail.com>
2023-07-15 13:43:38 +0330

    adding memleak in vsc++

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include "Engine_PCH.hpp"
 #include "GameInit.hpp"
-
 #include "PuzzleGame.hpp"
+
+//mem leak preprosesor only for msvc++
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif
+//mem leak preprosesor only for msvc++
 
 int main(int argc, char** argv)
 {
+#ifdef _DEBUG
+	//this is show mem leak if its on debug
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif // DEBUG
+
+	
 	//A_E stands for Ascii Engine
 	using namespace A_E;
 	PuzzleGame game{};
